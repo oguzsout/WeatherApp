9f97d49779cbd71786726d1079eb200b6fa4f68d
oguzsout <oguzd494@gmail.com>
2022-05-08 14:47:10 +0300

    Initial Commit

---
+++ Shared/WeatherIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIOApp.swift
+//  Shared
+//
+//  Created by oguz on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
