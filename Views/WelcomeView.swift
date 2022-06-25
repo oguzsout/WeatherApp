71475284a30cec7df96dec8aeb42c465c6b61c51
Oguzhan Dogdu <oguzd494@gmail.com>
2022-06-25 14:31:15 +0300

    finish welcome view

--- Views/WelcomeView.swift
+++ Views/WelcomeView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
     @EnvironmentObject var locationManager:LocationManager
@@ -14,7 +15,13 @@ struct WelcomeView: View {
             VStack(spacing:20){
                 Text("Welcome to the Weather App").bold().font(.title)
                 Text("Please share your current location to get the in your area").padding()
-            }.multilineTextAlignment(.center)
+            }.multilineTextAlignment(.center).padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }.cornerRadius(30)
+                .symbolVariant(.fill)
+                .foregroundColor(.white)
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
