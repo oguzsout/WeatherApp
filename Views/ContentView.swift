6a103716561b408900936048bfca8ca2d3c78c81
Oguzhan Dogdu <oguzd494@gmail.com>
2022-07-02 18:46:59 +0300

    loading view created

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -11,8 +11,17 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     var body: some View {
         VStack{
-            WelcomeView().environmentObject(locationManager)
-        }.background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+            if let location = locationManager.location{
+                Text("Your coordinates are: \(location.longitude),\(location.latitude)")
+                     } else {
+                         if locationManager.isLoading {
+                             LoadingView()
+                         } else {
+                             WelcomeView().environmentObject(locationManager)
+                             
+                         }
+                }
+            }.background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
             .preferredColorScheme(.dark)
     }
 }
---
+++ Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  WeatherIO
+//
+//  Created by Oguzhan Dogdu on 2.07.2022.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .white)).frame(maxWidth: .infinity,maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
