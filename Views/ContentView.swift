b508972926421dd26ab6d8ce3052dbb59229012f
oguzhandogdu <oguzd494@gmail.com>
2022-07-11 13:57:56 +0300

    ui connected with network

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -9,22 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
     var body: some View {
         VStack{
-            if let location = locationManager.location{
-                Text("Your coordinates are: \(location.longitude),\(location.latitude)")
-                     } else {
-                         if locationManager.isLoading {
-                             LoadingView()
-                         } else {
-                             WelcomeView().environmentObject(locationManager)
-                             
-                         }
+            if let location = locationManager.location {
+                            if let weather = weather {
+                               Text("Weather data fetched!")
+                            } else {
+                                LoadingView()
+                                    .task {
+                                        do {
+                                            weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                                        } catch {
+                                            print("Error getting weather: \(error)")
+                                        }
+                                    }
+                            }
+                        } else {
+                            if locationManager.isLoading {
+                                LoadingView()
+                            } else {
+                                WelcomeView()
+                                    .environmentObject(locationManager)
+                            }
+                        }
+                    }
+                    .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                    .preferredColorScheme(.dark)
                 }
-            }.background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
-            .preferredColorScheme(.dark)
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
